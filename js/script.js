4f40cf2cd0993089f308386e6a43981e04ce3f73
ThePlatypus-Person <Ornithorhynchus0920@protonmail.com>
2023-11-29 11:57:29 +0700

    Added shopping cart

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 angular.module('myApp', [])
     .controller('myController', ['$scope', function ($scope) {
 
+        // Adding Catalogue with JS
         data.forEach(item => createFeaturedBook(item));
         data.forEach(item => createNewArrivalItems(item, 1));
         data.reverse().forEach(item => createNewArrivalItems(item, 2));
@@ -28,6 +29,35 @@ angular.module('myApp', [])
                 document.querySelector('header .navbar').classList.remove('active');
             }
         };
+
+        // Add to Cart
+        $scope.cart = [];
+        let featuredItems = document.querySelectorAll('section.featured .featured-slider .box .content button');
+        featuredItems.forEach(item => {
+            item.addEventListener('click', (e) => {
+
+                const key = +e.target.dataset.key;
+                const selected = data.find(item => item.id === key);
+
+                const existing = $scope.cart.find(item => item.id === key);
+                const alreadyExist = (existing !== undefined);
+
+                if (alreadyExist) {
+                    existing["qty"] = existing.qty + 1;
+                } else {
+                    selected["qty"] = 1;
+                    $scope.cart.push(selected);
+                }
+                
+                const cart = document.querySelector('section.cart > .content');
+                cart.innerHTML = '';
+
+                $scope.cart.forEach(item => {
+                    addToCart(item);
+                });
+            });
+        });
+
 
         /*-------- swiper ---------- */
         var swiper = new Swiper(".books-list", {
@@ -128,7 +158,7 @@ function createFeaturedBook(obj) {
         <h3>${obj.title}</h3>
         <h4>Volume ${obj.volume}</h4>
         <div>
-            <a href="" class="btn">add to cart</a>
+            <button data-key="${obj.id}">add to cart</button>
             <div class="price">$${obj.price}</div>
         </div>
     `;
@@ -163,113 +193,175 @@ function createNewArrivalItems(obj, rowNum) {
     newArrivals.appendChild(item);
 }
 
+// Add to Cart UI
+function addToCart(obj) {
+    const cart = document.querySelector('section.cart > .content');
+
+    let item = document.createElement('div');
+    item.classList.add('item');
+
+    let image = document.createElement('img');
+    image.setAttribute('src', `${IMAGE_PATH}${obj.image}`);
+    item.appendChild(image);
+
+    let desc = document.createElement('div');
+    desc.classList.add('desc');
+
+    desc.innerHTML = `
+        <h4>${obj.title}</h4>
+        <h5>Volume ${obj.volume}</h5>
+        <p>Qty: ${obj.qty}</p>
+    `;
+    item.appendChild(desc);
+
+    cart.appendChild(item);
+}
+
+// Show Shopping Cart
+const shoppingCartBtn = document.querySelector('a.fas.fa-shopping-cart');
+shoppingCartBtn.addEventListener("click", () => {
+    const cart = document.querySelector('section.cart');
+    cart.classList.remove('slide-out-right');
+    cart.classList.add('slide-in-right');
+    cart.classList.toggle('active');
+});
+
+// Hide Shopping Cart
+const hideCartBtn = document.querySelector('a.fa-solid.fa-chevron-right');
+hideCartBtn.addEventListener("click", () => {
+    const cart = document.querySelector('section.cart');
+    cart.classList.remove('slide-in-right');
+    cart.classList.add('slide-out-right');
+    setTimeout(500, () => {
+        cart.classList.toggle('active');
+    });
+});
+
 
 const IMAGE_PATH = "./assets/manga/";
 
 // Comic dataset
 const data = [{
+    id: 1,
     title: "100 Ghost Stories",
     volume: "1",
     author: "Matono Anji",
     image: "ghost1.png",
     price: "9.99",
 }, {
+    id: 2,
     title: "100 Ghost Stories",
     volume: "2",
     author: "Matono Anji",
     image: "ghost2.jpg",
     price: "11.99",
 }, {
+    id: 3,
     title: "100 Ghost Stories",
     volume: "3",
     author: "Matono Anji",
     image: "ghost3.jpg",
     price: "9.99",
 }, {
+    id: 4,
     title: "100 Ghost Stories",
     volume: "4",
     author: "Matono Anji",
     image: "ghost4.jpg",
     price: "12.99",
 }, {
+    id: 5,
     title: "100 Ghost Stories",
     volume: "5",
     author: "Matono Anji",
     image: "ghost5.jpg",
     price: "11.99",
 }, {
+    id: 6,
     title: "100 Ghost Stories",
     volume: "6",
     author: "Matono Anji",
     image: "ghost6.jpg",
     price: "14.99",
 }, {
+    id: 7,
     title: "Goodbye, Eri",
     volume: "1",
     author: "Fujimoto Tatsuki",
     image: "goodbyeEri.jpg",
     price: "19.99",
 }, {
+    id: 8,
     title: "The Horizon",
     volume: "1",
     author: "Jeong Ji-Hoon",
     image: "theHorizon1.jpg",
     price: "10.99",
 }, {
+    id: 9,
     title: "The Horizon",
     volume: "2",
     author: "Jeong Ji-Hoon",
     image: "theHorizon2.jpg",
     price: "14.99",
 }, {
+    id: 10,
     title: "The Horizon",
     volume: "3",
     author: "Jeong Ji-Hoon",
     image: "theHorizon3.jpg",
     price: "19.99",
 }, {
+    id: 11,
     title: "Annarasumanara",
     volume: "1",
     author: "Ha Il-Kwon",
     image: "annarasumanara1.jpg",
     price: "9.99",
 }, {
+    id: 12,
     title: "Annarasumanara",
     volume: "2",
     author: "Ha Il-Kwon",
     image: "annarasumanara2.jpg",
     price: "8.99",
 }, {
+    id: 13,
     title: "Annarasumanara",
     volume: "3",
     author: "Ha Il-Kwon",
     image: "annarasumanara3.jpg",
     price: "6.99",
 }, {
+    id: 14,
     title: "Uzumaki",
     volume: "1",
     author: "Itou Junji",
     image: "uzumaki1.jpg",
     price: "11.99",
 }, {
+    id: 15,
     title: "Uzumaki",
     volume: "2",
     author: "Itou Junji",
     image: "uzumaki2.jpg",
     price: "13.99",
 }, {
+    id: 16,
     title: "Uzumaki",
     volume: "3",
     author: "Itou Junji",
     image: "uzumaki3.jpg",
     price: "12.99",
 }, {
+    id: 17,
     title: "Stargazing Dog",
     volume: "1",
     author: "Murakami Takashi",
     image: "stargazingDog1.jpg",
     price: "12.99",
 }, {
+    id: 18,
     title: "Stargazing Dog",
     volume: "2",
     author: "Murakami Takashi",
